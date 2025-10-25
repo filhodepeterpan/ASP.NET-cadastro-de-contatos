83aabf5ca3fd738ea5d17264de4fe6d024ed5cc2
Douglas Soares <douglassoares.cinema@gmail.com>
2025-10-25 14:21:09 -0300

    corrigindo solucao

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,31 +1,12 @@
-using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-using primeira_aplicacao_asp.net_mvc.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
-namespace primeira_aplicacao_asp.net_mvc.Controllers
+namespace CadastroDeContatos.Controllers
 {
     public class HomeController : Controller
     {
-
         public IActionResult Index()
         {
-            HomeModel home = new HomeModel();
-
-            home.Nome = "Douglas";
-            home.Email = "douglassoares.cinema@gmail.com";
-
-            return View(home);
-        }
-
-        public IActionResult Privacy()
-        {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- Models/HomeModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace primeira_aplicacao_asp.net_mvc.Models
-{
-    public class HomeModel
-    {
-        public string Nome { get; set; }
-        public string Email { get; set; }
-    }
-}
