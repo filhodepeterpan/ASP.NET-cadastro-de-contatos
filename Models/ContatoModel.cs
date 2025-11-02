6248c7dcab04a549d81f80bf00a3afcd5f63528b
Douglas Soares <douglassoares.cinema@gmail.com>
2025-11-02 15:08:24 -0300

    validando email e celular

--- Models/ContatoModel.cs
+++ Models/ContatoModel.cs
@@ -7,12 +7,14 @@ namespace CadastroDeContatos.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "O nome é de preenchimento obrigatório.")] 
-        public string Nome { get; set; }
+            public string Nome { get; set; }
 
-        [Required(ErrorMessage = "O email é de preenchimento obrigatório.")] 
-        public string Email { get; set; }
+        [Required(ErrorMessage = "O email é de preenchimento obrigatório.")]
+        [EmailAddress(ErrorMessage = "O email informado é inválido.")]
+            public string Email { get; set; }
 
-        [Required(ErrorMessage = "O celular é de preenchimento obrigatório.")] 
-        public string Celular { get; set; }
+        [Required(ErrorMessage = "O celular é de preenchimento obrigatório.")]
+        [Phone(ErrorMessage = "O celular informado é inválido.")]
+            public string Celular { get; set; }
     }
 }
